114fb1088f611bf00b3e060867015957e473658c
hazukieq <hazukie@qq.com>
2023-07-24 23:34:22 +0800

    updated all

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python ./main_test_swinir.py --task lightweight_sr  --scale 2 --noise 25 --model_path ./pretrained_models/002_lightweightSR_DIV2K_s64w8_SwinIR-S_x2.pth --folder_gt ../en_imgs
+python ./main_test_swinir.py --task lightweight_sr  --scale 2 --noise 25 --model_path ./pretrained_models/002_lightweightSR_DIV2K_s64w8_SwinIR-S_x2.pth --folder_lq ../en_imgs
 
 
 
